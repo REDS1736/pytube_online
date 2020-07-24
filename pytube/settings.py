2d9ce1547276b53ba91b9bbdb35037b4e326c2f3
Max <1024@mailbox.org>
2020-07-24 18:18:57 +0200

    add localhost to allowed hosts

--- pytube/settings.py
+++ pytube/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = secretstuff.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pytuber.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'pytuber.pythonanywhere.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
