21e1899aa6ae5d541f5e8fb83fde2d74614e52e2
Max <1024@mailbox.org>
2020-07-24 18:33:22 +0200

    basic skeleton for downloader app

--- downloader/urls.py
+++ downloader/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from downloader import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+    path('download', views.download, name='download')
 ]
--- downloader/views.py
+++ downloader/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'downloader/index.html')
+
+
+def download(request):
+    args = {
+        'url': request.POST.get('url', '{missing url}')
+    }
+    return render(request, 'downloader/download.html', args)
